873081a75feea19bf0c3049cccdaca4284e55c38
david <dabenegu@gmail.com>
2024-12-04 13:34:15 +0100

    kobe_bryant

--- GestionProyectos/src/main/java/com/prueba/controller/RouterController.java
+++ GestionProyectos/src/main/java/com/prueba/controller/RouterController.java
@@ -2,7 +2,6 @@ package com.prueba.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -19,9 +18,7 @@ public class RouterController {
     }
 
     @GetMapping("/projectAnalyzer")
-    public String getMethodName(@RequestParam String param) {
+    public String projectAnalyzer(){
         return "projectAnalyzer";
     }
-    
-
 }
